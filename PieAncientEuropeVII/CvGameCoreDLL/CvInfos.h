ce621056713526be67a292909260937e578fc2f7
dead-flunky <vale_mees@web.de>
2021-12-11 14:18:44 +0100

    Revert "Cont. dynamic unit limit."
    This reverts commit 996dc89000e90a7caaf16cc573e5d2723340833c.

--- PieAncientEuropeVII/CvGameCoreDLL/CvInfos.h
+++ PieAncientEuropeVII/CvGameCoreDLL/CvInfos.h
@@ -360,7 +360,6 @@ public:
 	int* getCommerceModifierArray() const; // K-Mod
 	int getSpecialistExtraCommerce(int i) const; // K-Mod, Exposed to Python
 	int* getSpecialistExtraCommerceArray() const; // K-Mod
-	int getUnitExtraPlayerInstances(int i) const; // Flunky
 	bool isCommerceFlexible(int i) const;	// Exposed to Python
 	bool isTerrainTrade(int i) const;			// Exposed to Python
 
@@ -428,7 +427,6 @@ protected:
 
 	int* m_piCommerceModifier; // K-Mod
 	int* m_piSpecialistExtraCommerce; // K-Mod
-	int* m_piUnitExtraPlayerInstances; // Flunky
 	bool* m_pbCommerceFlexible;
 	bool* m_pbTerrainTrade;
 
